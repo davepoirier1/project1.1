0740d706d0c5bcf155a70efdc9bab1a8d87284b1
Prelang Builder <builder@prelang.com>
2014-03-26 00:59:33 +0000

    Defined validation for attribute 'user_phone' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   validates_formatting_of :my_url, using: :url
   validates_formatting_of :uer_email, using: :email
+  validates_formatting_of :user_phone, using: :us_phone
 end
